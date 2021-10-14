c57ee066b1ff926cd8e6d860ca31c2b54603b9da
Rosso <>
2021-10-15 01:05:52 +1100

    Update init.lua

--- SKSE/Plugins/JCData/Lua/SexTats/init.lua
+++ SKSE/Plugins/JCData/Lua/SexTats/init.lua
@@ -10,16 +10,6 @@ function SexTats.getValidRandom(collection, last)
     return array[math.random(#array)]
 end
 
-function SexTats.handleSlaveTats2(collection)
-	local array = jc.filter(collection.Default, function(x) return x end) -- this is a weird hack
-	array = array[1]
-	local test = JMap.allKeys(array)
-	--local ret = array[test[1]]
-	--local ret = JArray.object()
-	local test1 = test[5]
-	return array[test1]
-end
-
 function SexTats.handleSlaveTats(collection)
 	local array = jc.filter(collection.Default, function(x) return x end) -- this is a weird hack
 	array = array[1]
@@ -33,11 +23,15 @@ function SexTats.handleSlaveTats(collection)
 		local mylen = #v
 		JMap.__newindex(extra, 'len', mylen)
 		JArray.insert(temp, extra)
-		JMap.__newindex(ret, k .. 'a', temp)
+		--JMap.__newindex(ret, k, temp)
 		local x = 1
 		while x < #v do
 			local tempx = temp[x]
-			JMap.__newindex(tempx, 'enabled', 'true')
+			JMap.__newindex(tempx, 'enabled', 1)
+			local section = JValue.solvePath(tempx, '.section'):gsub('%W','')
+			local area = JValue.solvePath(tempx, '.area'):gsub('%W','')
+			local name = JValue.solvePath(tempx, '.name'):gsub('%W','')
+			JMap.__newindex(ret, area .. '_' .. section .. '_' .. name .. '_' .. x, tempx)
 			x = x + 1
 		end
 	end
@@ -45,61 +39,5 @@ function SexTats.handleSlaveTats(collection)
 	return ret
 end
 
-function SexTats.handleSlaveTats3(collection)
-	local array = jc.filter(collection.Default, function(x) return x end) -- this is a weird hack
-	array = array[1]
-	local test = JMap.allKeys(array)
-	local test1 = test[5]
-	local ret = JMap.object()
-	
-	for k,v in pairs(array) do
-		local temp = v
-		local extra = JMap.object()
-		local mylen = #v
-		JMap.__newindex(extra, 'len', mylen)
-		JArray.insert(temp, extra)
-		JMap.__newindex(ret, k .. 'a', temp[mylen])
-	end
-		--JArray.insert(ret, array[test[x]])
-	return ret
-end
-
-function SexTats.b(collection)
-	local array = jc.filter(collection.Default, function(x) return x end) -- this is a weird hack
-	array = array[1]
-	local test = JMap.allKeys(array)
-	array = JValue.solvePath(array, '.' .. test[1])
-	array = array[1]
-	local test1 = JMap.object()
-	local test2 = AnalThisAnusOpen
-	--test1.AnalThisAnusOpen = array
-	JArray.insert(test1, array, tostring(test2))
-	
-	-- works
-	for k,v in pairs(array) do
-		if true == true then JMap.__newindex(ret, k, v) end
-	end
-	
-		for x,y in pairs(v) do
-			JMap.__newindex(ret, k, v[x])
-		end
-	
-	return test1
-end
-
--- returns a jcontainers object containing all objects with key value pair
--- JValue.evalLuaObj(animations, "return ostim.getAnimationsKeyValuePair(jobject,'animclass', 'Sx', 1, 0)")
--- papyrus bool to lua bool is an absolute pita, so just gonna use 1 and 0. if somebody works out something neater feel free to change.
-function SexTats.getAnimationsKeyValuePair(collection, key, value, allowPartialString, negativePartial)
-    return jc.filter(collection, function(x)
-        if allowPartialString == 1 then
-            local ret = true
-            if negativePartial == 1 then ret = false end
-            if ((x[key]):lower()):find((value):lower(), 1, true) then return ret else return not ret end
-        else
-            return x[key] == value
-        end
-    end)
-end
 
 return SexTats
